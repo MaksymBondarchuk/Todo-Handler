4b9bebf2b5c472b2847a639713d3f4bdbdc1f9ad
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2017-01-20 02:21:03 +0200

    With JSON

---
+++ Todo Handler/JsonObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Todo_Handler
+{
+    public class JsonObject
+    {
+        public List<TodoItem> Items { get; set; } = new List<TodoItem>();
+    }
+}
--- Todo Handler/MainWindow.xaml.cs
+++ Todo Handler/MainWindow.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Web.Script.Serialization;
 
 namespace Todo_Handler
 {
@@ -15,15 +16,32 @@ namespace Todo_Handler
         private const int Offset = 10;
         private const int GridHeight = 56;
         private const int GridWidth = 249;
+        private const string FileName = "Data.json";
 
         private int CurrentHorizontalOffset { get; set; } = 10;
         private int CurrentVerticalOffset { get; set; } = 10;
 
-        private List<TodoItem> Items { get; set; } = new List<TodoItem>();
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
+        private JsonObject Items { get; set; } = new JsonObject();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                using (var file = new StreamReader(FileName))
+                {
+                    var json = file.ReadToEnd();
+                    var deserializer = new JavaScriptSerializer();
+                    Items = deserializer.Deserialize<JsonObject>(json);
+                    AddAll();
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
 
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
@@ -38,7 +56,7 @@ namespace Todo_Handler
             if (string.IsNullOrEmpty(window.TodoTitle)) return;
             var id = Guid.NewGuid();
             AddItem(window.TodoTitle, window.Note, id);
-            Items.Add(new TodoItem { Title = window.TodoTitle, Note = window.Note, Id = id });
+            Items.Items.Add(new TodoItem { Title = window.TodoTitle, Note = window.Note, Id = id });
         }
 
         private void AddItem(string title, string note, Guid id)
@@ -75,22 +93,7 @@ namespace Todo_Handler
             };
             buttonEdit.Click += ButtonEdit_Click;
             grid.Children.Add(buttonEdit);
-            /*
-                <Button x:Name="button" Content="Delete" HorizontalAlignment="Left" Margin="164,10,0,0" VerticalAlignment="Top" Width="75"/>
-                <Label x:Name="label" Content="Label" HorizontalAlignment="Left" VerticalAlignment="Top" FontSize="14" FontWeight="Bold" Width="159" HorizontalContentAlignment="Center"/>
-                <Label x:Name="label1" Content="Label" HorizontalAlignment="Left" Margin="0,31,0,0" VerticalAlignment="Top" Width="159"/>
-                <Button x:Name="ButtonEdit" Content="Edit" HorizontalAlignment="Left" Margin="164,35,0,0" VerticalAlignment="Top" Width="75"/>
-               */
-            //grid.Children.Add(new TextBox
-            //{
-            //    Margin = new Thickness(10, 10, 0, 0),
-            //    HorizontalAlignment = HorizontalAlignment.Left,
-            //    VerticalAlignment = VerticalAlignment.Top,
-            //    Height = 23,
-            //    Width = 120,
-            //    TextWrapping = TextWrapping.Wrap,
-            //    Text = title
-            //});
+
             grid.Children.Add(new Label()
             {
                 HorizontalAlignment = HorizontalAlignment.Left,
@@ -132,7 +135,8 @@ namespace Todo_Handler
             if (btn == null)
                 return;
             var parentGrid = btn.Parent as Grid;
-            Items.Remove(Items.Select(t => t).FirstOrDefault(t => parentGrid != null && t.Id == (Guid)parentGrid.Tag));
+            Items.Items.Remove(Items.Items.Select(t => t)
+                .FirstOrDefault(t => parentGrid != null && t.Id == (Guid)parentGrid.Tag));
 
             //MainGrid.Children.Remove(parentGrid);
             DeleteAll();
@@ -145,7 +149,7 @@ namespace Todo_Handler
             var parentGrid = btn?.Parent as Grid;
             if (parentGrid == null) return;
 
-            var item = Items.Select(t => t).FirstOrDefault(t => t.Id == (Guid)parentGrid.Tag);
+            var item = Items.Items.Select(t => t).FirstOrDefault(t => t.Id == (Guid)parentGrid.Tag);
             if (item == null) return;
 
             var window = new EditWindow(item.Title, item.Note);
@@ -168,7 +172,7 @@ namespace Todo_Handler
 
         private void AddAll()
         {
-            foreach (var item in Items)
+            foreach (var item in Items.Items)
                 AddItem(item.Title, item.Note, item.Id);
         }
 
@@ -177,6 +181,15 @@ namespace Todo_Handler
             MainGrid.Children.Clear();
             CurrentHorizontalOffset = Offset;
             CurrentVerticalOffset = Offset;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var json = new JavaScriptSerializer().Serialize(Items);
+            using (var file = new StreamWriter(FileName))
+            {
+                file.WriteLine(json);
+            }
         }
     }
 }
